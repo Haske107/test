9ecae878fdbc2a8939575cb3db0ee54117f5769b
Jeff <Haske107@mail.chapman.edu>
2018-01-26 16:59:39 -0700

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
+
+  @ViewChild('videoPlayer') videoplayer: any;
+
+  toggleVideo(event: any) {
+    this.videoplayer.nativeElement.play();
+  }
+
+  ngOnInit()  {
+  }
+
+  ngAfterViewInit() {
+    this.toggleVideo(null);
+
+  }
 }
