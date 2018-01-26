4211c64a118b31ae8baf42d8cd6aea0045a454c5
Jeff <Haske107@mail.chapman.edu>
2018-01-26 14:39:23 -0700

    s

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
+router.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
 router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 module.exports = router;
