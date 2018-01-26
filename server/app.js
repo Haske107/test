379b89fe45d00179598a3daa3580eee0c3d137e8
Jeff <Haske107@mail.chapman.edu>
2018-01-26 14:43:01 -0700

    s

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,10 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-
 router.get('/', function (req, res, next) {
   res.render('index');
 });
+
 
 
 module.exports = router;
