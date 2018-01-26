c92904f04adbd54db48b9630df3c0124be4c08db
Jeff <Haske107@mail.chapman.edu>
2018-01-26 16:39:55 -0700

    s

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,8 @@ const path = require('path');
 // });
 
 router.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '../../../dist/index.html'));
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname + '../../dist/index.html'));
 });
 
 module.exports = router;
