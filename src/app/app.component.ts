3cb09b949edb790cc080c101729834faaf44d1f0
Jeff <Haske107@mail.chapman.edu>
2018-01-29 01:57:26 -0700

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,37 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.css','./app.component.phone.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  title = 'app';
+
+  state = 'block';
+  Left = '0%';
+  bottom = '100px';
+
+  opacity = 0;
+  blurr = 100;
+  scale = 0;
 
   @ViewChild('videoPlayer') videoplayer: any;
 
+  show = true;
+
+  borderCircle = '#363636';
+  borderSquare = '#363636';
+  borderTriangle = '#363636';
+
+  ContactLeft = '-500px';
+  MottoLeft = '0' ;
+
+  infoOpacity = '0';
+  comingOpacity = '0';
+
   toggleVideo(event: any) {
-    console.log(this.videoplayer);
     if(!this.videoplayer.nativeElement.paused)  {
-      this.videoplayer.nativeElement.pause();
+      if(this.videoplayer.nativeElement.muted)  {
+        this.videoplayer.nativeElement.muted = false;
+
+      } else {
+        this.videoplayer.nativeElement.pause();
+      }
     } else {
       this.videoplayer.nativeElement.play();
     }
@@ -22,7 +45,78 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit()  {
   }
 
+  toggleHider() {
+    this.toggleSquare();
+    if(this.state === 'block')  {
+      this.state = 'none';
+      this.Left = '-110%';
+      this.videoplayer.nativeElement.muted = true;
+      this.videoplayer.nativeElement.pause();
+      this.scale = 100;
+      this.opacity = 1;
+      this.blurr = 0;
+    } else  {
+      this.videoplayer.nativeElement.muted = false;
+      this.videoplayer.nativeElement.play();
+
+      this.state = 'block';
+      this.Left = '0%';
+      this.scale = 0;
+      this.opacity = 0;
+      this.blurr = 100;
+    }
+  }
+
   ngAfterViewInit() {
+    this.videoplayer.nativeElement.muted = true;
     this.videoplayer.nativeElement.play();
   }
+
+
+
+
+  toggleCircle()  {
+    if(this.borderCircle === '#363636') {
+      this.ContactLeft = '10%';
+      this.MottoLeft = '0%';
+      this.borderCircle = 'whitesmoke';
+    } else{
+      this.borderCircle = '#363636';
+      this.ContactLeft = '-500px';
+      this.MottoLeft = '0';
+    }
+  }
+
+  toggleTriangle()  {
+    if(this.show === true) {
+      this.show = false;
+      this.bottom = '-100%';
+      this.comingOpacity = '0';
+      this.videoplayer.nativeElement.muted = true;
+      this.videoplayer.nativeElement.pause();
+
+    } else{
+      this.show = true;
+      this.bottom = '100px';
+      this.comingOpacity = '1';
+      this.videoplayer.nativeElement.muted = false;
+      this.videoplayer.nativeElement.play();
+
+    }
+  }
+
+  toggleSquare()  {
+    if(this.borderSquare === '#363636') {
+      this.comingOpacity = '0';
+      this.infoOpacity = '1';
+      this.borderSquare = 'whitesmoke';
+    } else{
+      this.borderSquare = '#363636';
+      this.comingOpacity = '0';
+      this.infoOpacity = '0';
+
+    }
+  }
+
+
 }
